b56063e70653486169a4b7c8671475c1459ba99e
X-O1 <series-x-01@protonmail.com>
2023-07-17 17:50:40 -0700

    Added site feedback

--- src/app.js
+++ src/app.js
@@ -1,159 +1,206 @@
 // Imports
 const { ethers, BigNumber } = require("ethers");
 
-// Ethers.js state variables
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const abi = [
-  {
-    inputs: [],
-    stateMutability: "nonpayable",
-    type: "constructor",
-  },
-  {
-    inputs: [],
-    name: "ChooseWinner_TransferFailed",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "Deposit_Failed",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "Lottery__NotOwner",
-    type: "error",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: "address",
-        name: "player",
-        type: "address",
-      },
-    ],
-    name: "EnteredLottery",
-    type: "event",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: "address",
-        name: "player",
-        type: "address",
-      },
-    ],
-    name: "WinnerSelected",
-    type: "event",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "fundingAddress",
-        type: "address",
-      },
-    ],
-    name: "checkIfPlayerEntered",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "enterLottery",
-    outputs: [],
-    stateMutability: "payable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "getListOfPlayers",
-    outputs: [
-      {
-        internalType: "address payable[]",
-        name: "",
-        type: "address[]",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "getLotteryEndingThreshold",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "pure",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "getRoomLeftInPool",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-];
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-const signer = provider.getSigner();
-const entryFee = BigNumber.from("250000000000000000");
-
 // Front-end elements
 const walletConnectButton = document.querySelector(".wallet");
 const lotteryBalance = document.querySelector(".lottery-balance");
 const enterLotteryButton = document.querySelector(".enter-lottery-button");
 
+// Everytime site loads these functions will run first
 window.onload = () => {
   updateLotteryBalance();
+  updateFrontEndOnLoad();
 };
-// Connect site to a node (MetaMask)
-const connect = async () => {
-  if (window.ethereum !== "undefined") {
-    try {
-      await ethereum.request({ method: "eth_requestAccounts" });
 
-      ethereum.on("accountsChanged", (newAccounts) => {
-        const playerAccounts = getListOfPlayers();
+// Returns list of all players addresses that have entered Lottery
+const getListOfPlayers = async () => {
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const abi = [
+    {
+      inputs: [],
+      stateMutability: "nonpayable",
+      type: "constructor",
+    },
+    {
+      inputs: [],
+      name: "ChooseWinner_TransferFailed",
+      type: "error",
+    },
+    {
+      inputs: [],
+      name: "Deposit_Failed",
+      type: "error",
+    },
+    {
+      inputs: [],
+      name: "Lottery__NotOwner",
+      type: "error",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "player",
+          type: "address",
+        },
+      ],
+      name: "EnteredLottery",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "player",
+          type: "address",
+        },
+      ],
+      name: "WinnerSelected",
+      type: "event",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "fundingAddress",
+          type: "address",
+        },
+      ],
+      name: "checkIfPlayerEntered",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "enterLottery",
+      outputs: [],
+      stateMutability: "payable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "getListOfPlayers",
+      outputs: [
+        {
+          internalType: "address payable[]",
+          name: "",
+          type: "address[]",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "getLotteryEndingThreshold",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "pure",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "getRoomLeftInPool",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+  ];
+  const contract = new ethers.Contract(contractAddress, abi, provider);
+  const listOfPlayers = await contract.getListOfPlayers();
+  return listOfPlayers.map((player) => player.toLowerCase());
+};
+
+// Updates front-end everytime page reloads based on current wallet connected
+const updateFrontEndOnLoad = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const playerAccounts = await getListOfPlayers();
+
+  if (typeof window.ethereum !== undefined) {
+    try {
+      const connectedAddress = await signer.getAddress();
+      const connectedAddressLowerCase = connectedAddress.toLowerCase();
+      const accountEntered = await playerAccounts.includes(
+        connectedAddressLowerCase
+      );
+      if (connectedAddress === undefined) {
+        walletConnectButton.innerHTML = "Connect Wallet";
+        enterLotteryButton.innerHTML = "Enter Lottery!";
+        enterLotteryButton.style.fontSize = "46px";
+      } else if (accountEntered) {
+        updateFrontEnd();
+        walletConnectButton.innerHTML = "Connected";
+      } else {
+        enterLotteryButton.innerHTML = "Enter Lottery!";
+        enterLotteryButton.style.fontSize = "46px";
+        walletConnectButton.innerHTML = "Connected";
+      }
+    } catch (error) {
+      error = console.log("ERROR: No wallet is Connected.");
+    }
+  } else {
+    walletConnectButton.innerHTML = "Please install Metamask";
+  }
+};
+
+// Updates front-end every time wallet changes
+const updateFrontEndEveryTimeWalletChanges = async () => {
+  if (typeof window.ethereum !== undefined) {
+    try {
+      ethereum.on("accountsChanged", async (newAccounts) => {
+        const playerAccounts = await getListOfPlayers();
+        const accountEntered = await playerAccounts.includes(newAccounts[0]);
         console.log("Accounts changed", newAccounts[0]);
 
-        if (newAccounts[0] == undefined) {
+        if (newAccounts[0] === undefined) {
           walletConnectButton.innerHTML = "Connect Wallet";
           enterLotteryButton.innerHTML = "Enter Lottery!";
           enterLotteryButton.style.fontSize = "46px";
-        }
-
-        for (let i = 0; i < playerAccounts.length; i++) {
-          if (newAccounts[0] !== playerAccounts[i]) {
-            enterLotteryButton.innerHTML = "Enter Lottery!";
-            enterLotteryButton.style.fontSize = "46px";
-          } else {
-            updateFrontEnd();
-          }
+        } else if (accountEntered) {
+          updateFrontEnd();
+        } else {
+          enterLotteryButton.innerHTML = "Enter Lottery!";
+          enterLotteryButton.style.fontSize = "46px";
         }
       });
+    } catch (error) {
+      error = console.log("ERROR: No wallet is Connected.");
+    }
+  } else {
+    walletConnectButton.innerHTML = "Please install Metamask";
+  }
+};
+updateFrontEndEveryTimeWalletChanges();
 
+// Connects site to a node (MetaMask)
+const connect = async () => {
+  if (typeof window.ethereum !== "undefined") {
+    try {
+      await ethereum.request({ method: "eth_requestAccounts" });
       walletConnectButton.innerHTML = "Connected";
     } catch (error) {
       console.log(error);
@@ -165,8 +212,126 @@ const connect = async () => {
 
 // Enter the Lottery
 const execute = async () => {
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const entryFee = BigNumber.from("250000000000000000");
+  const abi = [
+    {
+      inputs: [],
+      stateMutability: "nonpayable",
+      type: "constructor",
+    },
+    {
+      inputs: [],
+      name: "ChooseWinner_TransferFailed",
+      type: "error",
+    },
+    {
+      inputs: [],
+      name: "Deposit_Failed",
+      type: "error",
+    },
+    {
+      inputs: [],
+      name: "Lottery__NotOwner",
+      type: "error",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "player",
+          type: "address",
+        },
+      ],
+      name: "EnteredLottery",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "player",
+          type: "address",
+        },
+      ],
+      name: "WinnerSelected",
+      type: "event",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "fundingAddress",
+          type: "address",
+        },
+      ],
+      name: "checkIfPlayerEntered",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "enterLottery",
+      outputs: [],
+      stateMutability: "payable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "getListOfPlayers",
+      outputs: [
+        {
+          internalType: "address payable[]",
+          name: "",
+          type: "address[]",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "getLotteryEndingThreshold",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "pure",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "getRoomLeftInPool",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+  ];
   const contract = new ethers.Contract(contractAddress, abi, signer);
-  if (window.ethereum !== "undefined") {
+
+  if (typeof window.ethereum !== "undefined") {
     try {
       const overrides = {
         value: entryFee,
@@ -178,7 +343,7 @@ const execute = async () => {
       updateLotteryBalance();
       updateFrontEnd();
     } catch (error) {
-      console.log(error);
+      error = console.log("This address has already entered the Lottery.");
     }
   } else {
     walletConnectButton.innerHTML = "Please install Metamask";
@@ -189,16 +354,18 @@ const updateFrontEnd = () => {
   enterLotteryButton.innerHTML = "Entered! Best of Luck!";
   enterLotteryButton.style.fontSize = "39px";
 };
-const getListOfPlayers = () => {
-  const contract = new ethers.Contract(contractAddress, abi, provider);
-  const listOfPlayers = contract.getListOfPlayers();
-  return listOfPlayers;
-};
+
 const updateLotteryBalance = async () => {
-  const balance = await provider.getBalance(contractAddress);
-  lotteryBalance.innerHTML = `\nLottery Balance: ${ethers.utils.formatEther(
-    balance
-  )} Ether\n`;
+  try {
+    const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const balance = await provider.getBalance(contractAddress);
+    lotteryBalance.innerHTML = `\nLottery Balance: ${ethers.utils.formatEther(
+      balance
+    )} Ether\n`;
+  } catch (error) {
+    error = console.log("ERROR: Failed to retrieve Lottery Balance.");
+  }
 };
 
 module.exports = {
@@ -207,4 +374,6 @@ module.exports = {
   getListOfPlayers,
   updateFrontEnd,
   updateLotteryBalance,
+  updateFrontEndEveryTimeWalletChanges,
+  updateFrontEndOnLoad,
 };
