38ede2cf833af577419b9361cb82b4f942156e1f
X-O1 <series-x-01@protonmail.com>
2023-07-26 00:45:45 -0700

    Redesigning front-end

--- public/bundle.js
+++ public/bundle.js
@@ -28831,7 +28831,7 @@ const updateEnterLotteryButtonDisplay = async () => {
   enterLotteryButton.innerHTML = "Entered! Best of Luck!";
   mediaQuery.matches
     ? (enterLotteryButton.style.fontSize = "39px")
-    : (enterLotteryButton.style.fontSize = "32px");
+    : (enterLotteryButton.style.fontSize = "25px");
 };
 
 const updateLotteryBalance = async () => {
